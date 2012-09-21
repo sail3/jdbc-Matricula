589d53fcce2641c9e2eff9bb9ae6cf19a78c09e1
sail3 <mlosail3@gmail.com>
2012-09-21 15:57:14 -0500

    Creamos el proyecto en Netbeans

---
+++ source/Matricula/src/com/matricula/test/Matricula.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.matricula.test;
+
+/**
+ *
+ * @author dev09
+ */
+public class Matricula {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("primer proyecto en GIT.");
+  }
+}
