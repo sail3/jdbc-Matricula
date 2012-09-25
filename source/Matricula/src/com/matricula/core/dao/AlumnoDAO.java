d71e3bec686296f737d1ff73c200c45544010c3b
sail3 <mlosail3@gmail.com>
2012-09-25 15:25:19 -0500

    Agregado las funciones de consulta y registro de alumnos a la base de datos

---
+++ source/Matricula/src/com/matricula/core/dao/AlumnoDAO.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.matricula.core.dao;
+
+import com.matricula.core.db.AccesoDB;
+import com.matricula.core.to.AlumnoTO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author dev09
+ */
+public class AlumnoDAO {
+  
+  /**
+   * Este metodo realiza una consulta a la tabla alumno.
+   * 
+   * @param codigo criterio de filtrado para la cosulta, segun sea codigo.
+   * @return retorta un objeto de tipo alumno con los datos rescatados de la base de datos.
+   * @throws SQLException
+   * @throws InstantiationException
+   * @throws ClassNotFoundException
+   * @throws IllegalAccessException 
+   */
+  public AlumnoTO obtenerAlumno(int codigo) throws SQLException, InstantiationException, ClassNotFoundException, IllegalAccessException {
+    String sql = "SELECT * FROM Alumno WHERE int_alumid = ?";
+    PreparedStatement pst = AccesoDB.getConection().prepareStatement(sql);
+    ResultSet cdr = null;
+    pst.setInt(1, codigo);
+    cdr = pst.executeQuery();
+    AlumnoTO alumno = null;
+    while (cdr.next()) {
+      alumno = new AlumnoTO();
+      alumno.setCodigo(cdr.getInt(1));
+      alumno.setNombre(cdr.getString(2));
+      alumno.setRecibo(cdr.getString(3));
+    }
+    return alumno;
+  }
+  
+  /**
+   * Este metodo registra un alumno a la base de datos
+   * 
+   * @param alumno Objeto de tipo alumno con los valores a insertar a la base de datos.
+   * @return Retorna un entero con la clave del nuevo alumno generado.
+   * @throws InstantiationException
+   * @throws IllegalAccessException
+   * @throws ClassNotFoundException
+   * @throws SQLException 
+   */
+  public int registrarAlumno(AlumnoTO alumno) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+    int codigo = 0;
+    String sql = "INSERT INTO Alumno VALUES (null, ?, ?)";
+    ResultSet cdr = null;
+    PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+    ps.setString(1, alumno.getNombre());
+    ps.setString(2, alumno.getRecibo());
+    ps.executeUpdate();
+    cdr = ps.getGeneratedKeys();
+    while (cdr.next()) {
+      codigo = cdr.getInt(1);
+    }
+    return codigo;
+  }
+
+}
+
--- source/Matricula/src/com/matricula/test/Matricula.java
+++ source/Matricula/src/com/matricula/test/Matricula.java
@@ -4,7 +4,9 @@
  */
 package com.matricula.test;
 
+import com.matricula.core.dao.AlumnoDAO;
 import com.matricula.core.db.AccesoDB;
+import com.matricula.core.to.AlumnoTO;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +24,14 @@ public class Matricula {
     try {
       System.out.println("primer proyecto en GIT.");
       System.out.println( AccesoDB.getConection() == null);
+      AlumnoTO alumno = new AlumnoDAO().obtenerAlumno(1000);
+      System.out.println(alumno == null);
+      System.out.println(alumno.getCodigo());
+      System.out.println(alumno.getNombre());
+      System.out.println(alumno.getRecibo());
+      System.out.println("insertardo alumno.......");
+      System.out.println("la clave del nuevo usuario es: " + new AlumnoDAO().registrarAlumno(new AlumnoTO("test03", "recibotest")));
+      
     } catch (InstantiationException ex) {
       Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
     } catch (IllegalAccessException ex) {
