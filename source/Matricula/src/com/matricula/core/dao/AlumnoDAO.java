aec020e29bdbccae7eace20ecc8cfa39f8eb37b2
Gustavo Fernandez <Gustavo.27.92@hotmail.com>
2012-10-11 18:58:20 -0500

    removiendo casting innecesario y objeto ResultSet no usado.

--- source/Matricula/src/com/matricula/core/dao/AlumnoDAO.java
+++ source/Matricula/src/com/matricula/core/dao/AlumnoDAO.java
@@ -1,19 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.matricula.core.dao;
 
 import com.matricula.core.db.AccesoDB;
 import com.matricula.core.interfaces.AlumnoDAOInterface;
 import com.matricula.core.to.AlumnoTO;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,112 +13,107 @@ import java.util.List;
  *
  * @author dev09
  */
-public class AlumnoDAO implements AlumnoDAOInterface{
-  
-  @Override
-  public List<AlumnoTO> listarTodos() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-    String sql = "SELECT * FROM Alumnos";
-    PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql);
-    ResultSet cdr = ps.executeQuery();
-    ArrayList<AlumnoTO> lista = new ArrayList<AlumnoTO>();
-    while (cdr.next()) {
-      AlumnoTO alumno = null;
-      alumno.setCodigo(cdr.getInt(1));
-      alumno.setNombre(cdr.getString(2));
-      alumno.setRecibo(cdr.getString(3));
-      lista.add(alumno);
+public class AlumnoDAO implements AlumnoDAOInterface {
+
+    @Override
+    public List<AlumnoTO> listarTodos() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        String sql = "SELECT * FROM Alumnos";
+        PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql);
+        ResultSet cdr = ps.executeQuery();
+        ArrayList<AlumnoTO> lista = new ArrayList<AlumnoTO>();
+        while (cdr.next()) {
+            AlumnoTO alumno = null;
+            alumno.setCodigo(cdr.getInt(1));
+            alumno.setNombre(cdr.getString(2));
+            alumno.setRecibo(cdr.getString(3));
+            lista.add(alumno);
+        }
+        return lista;
     }
-    return lista;
-  }
 
-  @Override
-  public List<AlumnoTO> listarPorNombre(String nombre) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-    String sql = "SELECT * FROM Alumno WHERE vch_alumnombre LIKE '%?%'";
-    PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql);
-    ps.setString(1, nombre);
-    ResultSet cdr = ps.executeQuery();
-    ArrayList<AlumnoTO> lista = new ArrayList<AlumnoTO>();
-    while (cdr.next()) {
-      AlumnoTO alumno = null;
-      alumno.setCodigo(cdr.getInt(1));
-      alumno.setNombre(cdr.getString(2));
-      alumno.setRecibo(cdr.getString(3));
-      lista.add(alumno);
+    @Override
+    public List<AlumnoTO> listarPorNombre(String nombre) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        String sql = "SELECT * FROM Alumno WHERE vch_alumnombre LIKE '%?%'";
+        PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql);
+        ps.setString(1, nombre);
+        ResultSet cdr = ps.executeQuery();
+        ArrayList<AlumnoTO> lista = new ArrayList<AlumnoTO>();
+        while (cdr.next()) {
+            AlumnoTO alumno = null;
+            alumno.setCodigo(cdr.getInt(1));
+            alumno.setNombre(cdr.getString(2));
+            alumno.setRecibo(cdr.getString(3));
+            lista.add(alumno);
+        }
+        return lista;
     }
-    return lista;
-  }
 
-  @Override
-  public List<AlumnoTO> listarPorRecibo(String recibo) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-    String sql = "SELECT * FROM Alumno WHERE chr_alumrecibo LIKE '%?%'";
-    PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql);
-    ps.setString(1, recibo);
-    ResultSet cdr = ps.executeQuery();
-    ArrayList<AlumnoTO> lista = new ArrayList<AlumnoTO>();
-    while (cdr.next()) {
-      AlumnoTO alumno = null;
-      alumno.setCodigo(cdr.getInt(1));
-      alumno.setNombre(cdr.getString(2));
-      alumno.setRecibo(cdr.getString(3));
-      lista.add(alumno);
+    @Override
+    public List<AlumnoTO> listarPorRecibo(String recibo) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        String sql = "SELECT * FROM Alumno WHERE chr_alumrecibo LIKE '%?%'";
+        PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql);
+        ps.setString(1, recibo);
+        ResultSet cdr = ps.executeQuery();
+        ArrayList<AlumnoTO> lista = new ArrayList<AlumnoTO>();
+        while (cdr.next()) {
+            AlumnoTO alumno = null;
+            alumno.setCodigo(cdr.getInt(1));
+            alumno.setNombre(cdr.getString(2));
+            alumno.setRecibo(cdr.getString(3));
+            lista.add(alumno);
+        }
+        return lista;
     }
-    return lista;
-  }
 
-  @Override
-  public int crear(AlumnoTO obj) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-    int codigo = 0;
-    String sql = "INSERT INTO Alumno VALUES (null, ?, ?)";
-    ResultSet cdr = null;
-    PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-    ps.setString(1, obj.getNombre());
-    ps.setString(2, obj.getRecibo());
-    ps.executeUpdate();
-    cdr = ps.getGeneratedKeys();
-    while (cdr.next()) {
-      codigo = cdr.getInt(1);
+    @Override
+    public int crear(AlumnoTO obj) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        int codigo = 0;
+        String sql = "INSERT INTO Alumno VALUES (null, ?, ?)";
+        ResultSet cdr = null;
+        PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        ps.setString(1, obj.getNombre());
+        ps.setString(2, obj.getRecibo());
+        ps.executeUpdate();
+        cdr = ps.getGeneratedKeys();
+        while (cdr.next()) {
+            codigo = cdr.getInt(1);
+        }
+        return codigo;
     }
-    return codigo;
-  }
 
-  @Override
-  public AlumnoTO recuperar(Integer key) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-    String sql = "SELECT * FROM Alumno WHERE int_alumid = ?";
-    PreparedStatement pst = AccesoDB.getConection().prepareStatement(sql);
-    ResultSet cdr = null;
-    pst.setInt(1, key);
-    cdr = pst.executeQuery();
-    AlumnoTO alumno = null;
-    while (cdr.next()) {
-      alumno = new AlumnoTO();
-      alumno.setCodigo(cdr.getInt(1));
-      alumno.setNombre(cdr.getString(2));
-      alumno.setRecibo(cdr.getString(3));
+    @Override
+    public AlumnoTO recuperar(Integer key) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        String sql = "SELECT * FROM Alumno WHERE int_alumid = ?";
+        PreparedStatement pst = AccesoDB.getConection().prepareStatement(sql);
+        ResultSet cdr = null;
+        pst.setInt(1, key);
+        cdr = pst.executeQuery();
+        AlumnoTO alumno = null;
+        while (cdr.next()) {
+            alumno = new AlumnoTO();
+            alumno.setCodigo(cdr.getInt(1));
+            alumno.setNombre(cdr.getString(2));
+            alumno.setRecibo(cdr.getString(3));
+        }
+        return alumno;
     }
-    return alumno;
-  }
 
-  @Override
-  public int actualizar(AlumnoTO obj) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-    String sql = "UPDATE Alumno SET vch_alumnombre=?, chr_alumrecibo=? WHERE int_alumid=?";
-    PreparedStatement pst = AccesoDB.getConection().prepareStatement(sql);
-    ResultSet cdr = null; 
-    pst.setString(1, obj.getNombre());
-    pst.setString(2, obj.getRecibo());
-    pst.setInt(3, obj.getCodigo());
-    return (int)pst.executeUpdate();
-  }
+    @Override
+    public int actualizar(AlumnoTO obj) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        String sql = "UPDATE Alumno SET vch_alumnombre=?, chr_alumrecibo=? WHERE int_alumid=?";
+        PreparedStatement pst = AccesoDB.getConection().prepareStatement(sql);
+        pst.setString(1, obj.getNombre());
+        pst.setString(2, obj.getRecibo());
+        pst.setInt(3, obj.getCodigo());
+        return pst.executeUpdate();
+    }
 
-  @Override
-  public int eliminar(Integer key) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-    String sql = "DELETE FROM Alumno WHERE int_alumid = ?";
-    ResultSet cdr = null;
-    PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql);
-    ps.setInt(1, key);
-    return (int)ps.executeUpdate();
-  }
-
-
-
+    @Override
+    public int eliminar(Integer key) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        String sql = "DELETE FROM Alumno WHERE int_alumid = ?";
+        ResultSet cdr = null;
+        PreparedStatement ps = AccesoDB.getConection().prepareStatement(sql);
+        ps.setInt(1, key);
+        return ps.executeUpdate();
+    }
 }
-
--- source/Matricula/src/com/matricula/test/Matricula.java
+++ source/Matricula/src/com/matricula/test/Matricula.java
@@ -1,85 +1,89 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.matricula.test;
 
 import com.matricula.core.dao.AlumnoDAO;
-import com.matricula.core.db.AccesoDB;
 import com.matricula.core.to.AlumnoTO;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.io.*;
 
 /**
  *
  * @author dev09
  */
 public class Matricula {
-AlumnoDAO Objeto = new AlumnoDAO();
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) throws IOException {
-    try {
-      //Se crea una lectora de datos de tipo BufferedReader  
-      BufferedReader lec = new BufferedReader(new InputStreamReader(System.in));  
-      System.out.println("Primer proyecto en GIT.");
-      System.out.println("----------------------------------");
-      System.out.println("Menú de opciones:");//Mostramos las tres opciones de nuestro menú
-      System.out.println("1.--Crear un registro\n2.--Editar un registro\n3.--Salir");
-      System.out.println("----------------------------------");
-      System.out.println("Elija una opcion:");
-      int opcion=Integer.parseInt(lec.readLine());//Leemos la opcion y jugamos con el switch
-              
-      switch(opcion){
-                        
-          case 1://Creando un registro
-              System.out.println("----------------------------------");
-              System.out.println("  Ingrese informacion del alumno");
-              System.out.println("----------------------------------");//Pedimos los datos
-              System.out.println("Ingrese nombre:");String nombre=lec.readLine();
-              System.out.println("Ingrese recibo:");String recibo=lec.readLine();
-              //Llamamos al metodo registrar
-              new AlumnoDAO().crear(new AlumnoTO(nombre, recibo));
-              System.out.println("----------------------------------");
-              System.out.println("Registro creado!");
-              break;
-          
-          case 2: //Editando un registro
-              System.out.println("Ingrese su codigo: ");
-              int codigo=Integer.parseInt(lec.readLine());//Leemos el codigo
-              AlumnoTO a = new AlumnoDAO().recuperar(codigo);//Obtenemos la informacion del alumno
-              System.out.println("----------------------------------");//Mostramos la informacion del alumno
-              System.out.println("Codigo Actual:"+a.getCodigo()+"\nNombre Actual:"+a.getNombre()+"\nRecibo Actual:"+a.getRecibo());
-              System.out.println("----------------------------------");
-              System.out.println("Nuevo nombre:");String nuevoNombre=lec.readLine();
-              System.out.println("Nuevo recibo:");String nuevoRecibo=lec.readLine();
-              //Llamamos al metodo editar
-              new AlumnoDAO().actualizar(new AlumnoTO(codigo, nuevoNombre, nuevoRecibo));
-              System.out.println("----------------------------------");
-              System.out.println("Registro editado!");
-              break;
-          
-          case 3:
-              System.exit(0);
-              break;
-          
-          default: 
-              System.out.println("Opcion no valida!");
-              
-      }
-      
-    } catch (InstantiationException ex) {
-      Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (IllegalAccessException ex) {
-      Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (SQLException ex) {
-      Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (ClassNotFoundException ex) {
-      Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
+
+    AlumnoDAO Objeto = new AlumnoDAO();
+
+    /**
+     * @param args the command line arguments
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        try {
+            //Se crea una lectora de datos de tipo BufferedReader  
+            BufferedReader lec = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Primer proyecto en GIT.");
+            System.out.println("----------------------------------");
+            System.out.println("Menú de opciones:");//Mostramos las tres opciones de nuestro menú
+            System.out.println("1.--Crear un registro\n2.--Editar un registro\n3.--Salir");
+            System.out.println("----------------------------------");
+            System.out.println("Elija una opcion:");
+            int opcion = Integer.parseInt(lec.readLine());//Leemos la opcion y jugamos con el switch
+
+            switch (opcion) {
+
+                case 1://Creando un registro
+                    System.out.println("----------------------------------");
+                    System.out.println("  Ingrese informacion del alumno");
+                    System.out.println("----------------------------------");//Pedimos los datos
+                    System.out.println("Ingrese nombre:");
+                    String nombre = lec.readLine();
+                    System.out.println("Ingrese recibo:");
+                    String recibo = lec.readLine();
+                    //Llamamos al metodo registrar
+                    new AlumnoDAO().crear(new AlumnoTO(nombre, recibo));
+                    System.out.println("----------------------------------");
+                    System.out.println("Registro creado!");
+                    break;
+
+                case 2: //Editando un registro
+                    System.out.println("Ingrese su codigo: ");
+                    int codigo = Integer.parseInt(lec.readLine());//Leemos el codigo
+                    AlumnoTO a = new AlumnoDAO().recuperar(codigo);//Obtenemos la informacion del alumno
+                    System.out.println("----------------------------------");//Mostramos la informacion del alumno
+                    System.out.println("Codigo Actual:" + a.getCodigo() + "\nNombre Actual:" + a.getNombre() + "\nRecibo Actual:" + a.getRecibo());
+                    System.out.println("----------------------------------");
+                    System.out.println("Nuevo nombre:");
+                    String nuevoNombre = lec.readLine();
+                    System.out.println("Nuevo recibo:");
+                    String nuevoRecibo = lec.readLine();
+                    //Llamamos al metodo editar
+                    new AlumnoDAO().actualizar(new AlumnoTO(codigo, nuevoNombre, nuevoRecibo));
+                    System.out.println("----------------------------------");
+                    System.out.println("Registro editado!");
+                    break;
+
+                case 3:
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida!");
+
+            }
+
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Matricula.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
-  }
 }
